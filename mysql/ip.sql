363ffcfd2c8b9a8259e364c8cf70917551cd26cb
qqzeng-ip <zengxiangzhan2010@163.com>
2016-08-13 10:28:23 +0800

    Update ip.sql
    txt->mysql（导入和查询）

--- mysql/ip.sql
+++ mysql/ip.sql
@@ -33,16 +33,8 @@ LINES TERMINATED BY '\n'
 
 
 
---导入数据库
-LOAD DATA LOCAL INFILE 'G:\IP数据库\ip.txt'
-INTO TABLE ip
-FIELDS TERMINATED BY '|'
-LINES TERMINATED BY '\n'
-(StartIp, EndIp, StartIpNum,EndIpNum,Country,Province,City,District,ISP,AreaCode);
-
-
 --查询
-SELECT * FROM ip WHERE INET_ATON('219.232.57.199') BETWEEN StartIpNum AND EndIpNum LIMIT 1
+SELECT * FROM ip WHERE INET_ATON('219.232.57.199') BETWEEN ip_start_num AND ip_end_num LIMIT 1
 
 
 
