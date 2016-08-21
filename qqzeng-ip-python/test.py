ab2ee635bba41ca28d680af809062cafb68bde89
qqzeng-ip <zengxiangzhan2010@163.com>
2016-08-21 16:44:07 +0800

    最新一代文件结构 Python 解析
    最新一代文件结构 Python 解析

---
+++ qqzeng-ip-python/test.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from IpSearch import IpSearch
+
+qqzeng = IpSearch("qqzeng-ip-utf8.dat")
+result=qqzeng.Find("1.2.3.4")
+print result
+
+#result="亚洲|中国|香港|九龙|油尖旺|新世界电讯|810200|Hong Kong|HK|114.17495|22.327115"
