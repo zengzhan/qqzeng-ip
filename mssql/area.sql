9624b525b09c1409213a899646c138b08444f366
qqzeng-ip <zengxiangzhan2010@163.com>
2016-09-02 21:23:12 +0800

    txt->mssql（导入和查询）
    txt->mssql（导入和查询）

--- mssql/area.sql
+++ mssql/area.sql
@@ -1,19 +1,19 @@
 --附 MSSQL导入方法:
 
---创建 最新行政区划数据库
+--创建 最新行政区划数据库  旗舰版
 
---字段 区划ID-父ID-级别-全称-全称聚合-区号-邮编-简称-简称聚合-拼音-简拼-首字母-经度-纬度
+--字段 区划ID-父ID-全称-全称聚合-简称-简称聚合-级别-区号-邮编-拼音-简拼-首字母-经度-纬度-备注
 
 CREATE TABLE [dbo].[areas](
 	[ID] [NVARCHAR](50) NULL,
 	[ParentId] [NVARCHAR](50) NULL,
-	[LevelType] [NVARCHAR](50) NULL,
 	[Name] [NVARCHAR](50) NULL,
 	[MergerName] [NVARCHAR](200) NULL,
-	[CityCode] [NVARCHAR](50) NULL,
-	[ZipCode] [NVARCHAR](50) NULL,
 	[ShortName] [NVARCHAR](50) NULL,
 	[MergerShortName] [NVARCHAR](200) NULL,
+	[LevelType] [NVARCHAR](50) NULL,
+	[CityCode] [NVARCHAR](50) NULL,
+	[ZipCode] [NVARCHAR](50) NULL,
 	[Pinyin] [NVARCHAR](50) NULL,
 	[Jianpin] [NVARCHAR](50) NULL,
 	[FirstChar] [NVARCHAR](50) NULL,
