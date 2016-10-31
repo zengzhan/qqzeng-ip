e85225ff972e7a8f3f211eb50f1d410e25846b24
qqzeng-ip <zengxiangzhan2010@163.com>
2016-10-31 16:56:55 +0800

    txt->mysql（导入和查询）
    txt->mysql（导入和查询）

--- mysql/phone.sql
+++ mysql/phone.sql
@@ -19,9 +19,24 @@ CREATE TABLE `phone``.`phone` (
 --导入数据库
 LOAD DATA LOCAL INFILE 'G:\phone.txt'
 INTO TABLE phone
-FIELDS TERMINATED BY '|'
+FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
 (pref, phone, province,city,isp,code,zip,types);
 
 
 --默认 txt为中文编码  导入时 请转为utf-8编码 以免乱码 
+
+--mysql乱码如何解决？
+
+
+--解决：
+
+  --确保两者编码统一 才不会乱码
+
+--（1）先将txt文件转换为UTF-8格式 
+
+--（2）导入命令中加入character set utf8
+
+--如：
+
+…… into table test character set utf8 fields……
