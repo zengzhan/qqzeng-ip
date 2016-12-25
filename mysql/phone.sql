a313a07c0f092236b7de14f61fc2a4c5075057f5
qqzeng-ip <zengxiangzhan2010@163.com>
2016-12-25 23:10:53 +0800

    txt->mysql（导入和查询）
    txt->mysql（导入和查询）

--- mysql/phone.sql
+++ mysql/phone.sql
@@ -2,7 +2,7 @@
 
 --创建表 最新手机号段归属地数据库
 
---字段：前三位 号段 省份 城市 运营商 区号 邮编 类型
+--字段：前三位 号段 省份 城市 运营商  邮编 区号 行政区划编码 类型
 
 CREATE TABLE `phone``.`phone` (
 `pref`  VARCHAR(45) NULL,
@@ -10,8 +10,9 @@ CREATE TABLE `phone``.`phone` (
 `province` VARCHAR(45) NULL,
 `city` VARCHAR(45) NULL,
 `isp` VARCHAR(45) NULL,
-`code` VARCHAR(45) NULL,
-`zip` VARCHAR(45) NULL,
+`post_code` VARCHAR(45) NULL,
+`city_code` VARCHAR(45) NULL,
+`area_code` VARCHAR(45) NULL,
 `types` VARCHAR(45) NULL
 );
 
@@ -21,7 +22,8 @@ LOAD DATA LOCAL INFILE 'G:\phone.txt'
 INTO TABLE phone
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
-(pref, phone, province,city,isp,code,zip,types);
+(pref, phone, province,city,isp,post_code,city_code,area_code,types);
+
 
 
 --默认 txt为中文编码  导入时 请转为utf-8编码 以免乱码 
@@ -31,7 +33,7 @@ LINES TERMINATED BY '\n'
 
 --解决：
 
-  --确保两者编码统一 才不会乱码
+--确保两者编码统一 才不会乱码
 
 --（1）先将txt文件转换为UTF-8格式 
 
@@ -40,3 +42,6 @@ LINES TERMINATED BY '\n'
 --如：
 
 …… into table test character set utf8 fields……
+
+
+
