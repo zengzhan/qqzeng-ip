c0e5099be4490ad4142a19d26ef473c9235009ea
qqzeng-ip <zengxiangzhan2010@163.com>
2018-01-28 18:43:58 +0800

    txt->mssql（导入和查询）
    txt->mssql（导入和查询）

--- mssql/phone.sql
+++ mssql/phone.sql
@@ -2,7 +2,7 @@
 
 --创建表 最新手机号段归属地数据库
 
---字段：前三位 号段 省份 城市 运营商  邮编 区号 行政区划代码 类型
+--字段：前三位 号段 省份 城市 运营商类型  邮编 区号 行政区划代码
 
 CREATE TABLE [dbo].[PhoneLocation](
  [pref] [varchar](50) NULL,
@@ -12,8 +12,7 @@ CREATE TABLE [dbo].[PhoneLocation](
  [isp] [varchar](50) NULL,
  [post_code] [varchar](50) NULL,
  [city_code] [varchar](50) NULL,
- [area_code] [varchar](50) NULL,
- [types] [varchar](50) NULL
+ [area_code] [varchar](50) NULL
 )
 
 --导入数据库
