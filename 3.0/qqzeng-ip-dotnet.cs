a85f457ce48df3ac7d7377308b226262a32771b9
qqzeng-ip <zengxiangzhan2010@163.com>
2018-05-16 12:11:46 +0800

    最新一代3.0版全球旗舰版DAT结构，压缩率更高，每秒解析上千万
    最新一代3.0版全球旗舰版DAT结构，压缩率更高，每秒解析上千万

--- 3.0/qqzeng-ip-dotnet.cs
+++ 3.0/qqzeng-ip-dotnet.cs
@@ -285,6 +285,6 @@ namespace qqzeng_ip_dat
     /*
     （调用例子）：   
     string result = IPSearch3Fast.Instance.Find("1.2.3.4");
-   --> result="亚洲|中国|香港|九龙|油尖旺|新世界电讯|810200|Hong Kong|HK|114.17495|22.327115"
+   --> result="亚洲|中国|香港|九龙|油尖旺|新世界电讯|810200|Hong Kong|HK|114.17495|22.327115" 
     */
 }
