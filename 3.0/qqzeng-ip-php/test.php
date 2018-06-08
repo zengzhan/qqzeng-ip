d04a1c1544669a37a2e2b587cba5cfc44298bc54
qqzeng-ip <zengxiangzhan2010@163.com>
2018-06-08 23:18:42 +0800

    最新一代3.0版 php 解析
    最新一代3.0版 php 解析

---
+++ 3.0/qqzeng-ip-php/test.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: text/json;charset=utf-8"');
+include_once("IpSearch.php");
+
+$client_ip='8.8.8.8';
+$reader = new IpSearch('qqzeng-ip-3.0-ultimate.dat');
+
+$area_info = $reader->get($client_ip);
+
+list($continent,$country,$province, $city, $district,$isp,$areacode,$en,$cc,$lng,$lat) = explode("|", $area_info);
+
+$arr =array(
+			'code'=>0,
+				'data'=> array( 
+								'ip'=>$client_ip,
+								'continent'=>$continent,
+								'country'=>$country,
+								'province'=>$province,
+								'city'=>$city, 
+								'district'=>$district, 
+								'isp'=>$isp,
+								'areacode'=>$areacode,
+								'en'=>$en,
+								'cc'=>$cc,
+								'lng'=>$lng,
+								'lat'=>$lat
+								
+						)); 
+	echo   json_encode($arr); 
+	
+	//返回json数据 
+/* 
+	{"code":0,"data":{"ip":"8.8.8.8","continent":"\u5317\u7f8e\u6d32","country":"\u7f8e\u56fd","province":"","city":"","district":"","isp":"GoogleDNS","areacode":"","en":"United States","cc":"US","lng":"-95.712891","lat":"37.09024"}}
+ */
+?>
