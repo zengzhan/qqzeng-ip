38245c5eba162325bcf2dd18d0d638b407a33ef2
qqzeng-ip <zengxiangzhan2010@163.com>
2018-08-13 02:25:10 +0800

    txt->mysql（导入和查询）
    txt->mysql（导入和查询）

--- mysql/area.sql
+++ mysql/area.sql
@@ -1,36 +1,44 @@
---mysql 导入数据库 
+-- mysql 导入数据库 
 
---创建表 最新行政区划数据库
+-- 创建表 最新行政区划数据库
 
---字段 区划ID-父ID-全称-全称聚合-简称-简称聚合-级别-区号-邮编-拼音-简拼-首字母-经度-纬度-备注
+-- 字段 区划ID-父ID-全称-全称聚合-简称-简称聚合-级别-区号-邮编-拼音-简拼-首字母-经度-纬度-行政区-功能区
 
-CREATE TABLE `areas``.`areas` (
-`ID`  VARCHAR(45) NULL,
-`ParentId` VARCHAR(45) NULL,
-`Name` VARCHAR(45) NULL,
-`MergerName` VARCHAR(200) NULL,
-`ShortName` VARCHAR(45) NULL,
-`MergerShortName` VARCHAR(200) NULL,
-`LevelType` VARCHAR(45) NULL,
-`CityCode` VARCHAR(45) NULL,
-`ZipCode` VARCHAR(45) NULL,
-`Pinyin` VARCHAR(45) NULL,
-`Jianpin` VARCHAR(45) NULL,
-`FirstChar` VARCHAR(45) NULL,
-`lng` VARCHAR(45) NULL,
-`Lat` VARCHAR(45) NULL,
-`Remark` VARCHAR(45) NULL
+create table `zengip`.`areas` (
+    `ID` varchar(45) null,
+    `ParentId` varchar(45) null,
+    `Name` varchar(45) null,
+    `MergerName` varchar(200) null,
+    `ShortName` varchar(45) null,
+    `MergerShortName` varchar(200) null,
+    `LevelType` varchar(45) null,
+    `CityCode` varchar(45) null,
+    `ZipCode` varchar(45) null,
+    `Pinyin` varchar(45) null,
+    `Jianpin` varchar(45) null,
+    `FirstChar` varchar(45) null,
+    `lng` varchar(45) null,
+    `Lat` varchar(45) null,
+    `Remark1` varchar(45) null,
+    `Remark2` varchar(45) null
 );
 
 
 
---导入数据库
-LOAD DATA LOCAL INFILE 'G:\areas.txt'
-INTO TABLE areas
-FIELDS TERMINATED BY '\t'
-LINES TERMINATED BY '\n'
-(ID,ParentId,Name,MergerName,ShortName,MergerShortName,LevelType,CityCode,ZipCode,Pinyin,Jianpin,FirstChar,lng,Lat,Remark);
+
+-- 导入数据库
+load data local infile 'C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\areas.txt'
+into table areas
+fields terminated by '\t'  -- '|'
+lines terminated by '\n'
+(ID,ParentId,Name,MergerName,ShortName,MergerShortName,LevelType,CityCode,ZipCode,Pinyin,Jianpin,FirstChar,lng,Lat,Remark1,Remark2);
 
 
---xls 全选复制到txt
---默认 txt为中文编码  导入时 请转为utf-8编码 以免乱码 
+-- Error Code: 1148. The used command is not allowed with this MySQL version
+-- local-infile = 1 修改后必须重启mysql
+
+
+-- xls 全选复制到txt
+-- 默认 txt为中文编码  导入时 请转为utf-8编码 以免乱码 
+
+
