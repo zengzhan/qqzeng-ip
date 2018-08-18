55e0689723bea40a0bc874025ddebe3beb21fa73
qqzeng-ip <zengxiangzhan2010@163.com>
2018-08-18 18:43:42 +0800

    最新一代3.0版 php 解析
    最新一代3.0版 php 解析

--- 3.0/qqzeng-ip-php/IpSearch.php
+++ 3.0/qqzeng-ip-php/IpSearch.php
@@ -1,17 +1,30 @@
 <?php
 /* 
 PHP Version 7.2.2
-qqzeng-ip 3.0 2018-06-08
-*/
-class IpSearch {
+qqzeng-ip 3.0 2018-08-18
 
+在Java C# 中单例会一直存在于整个应用程序的生命周期里，变量是跨页面级的，真正可以做到这个实例在应用程序生命周期中的唯一性。
+然而在PHP中，所有的变量无论是全局变量还是类的静态成员，都是页面级的，每次页面被执行时，都会重新建立新的对象，都会在页面执行完毕后被清空
+
+
+*/
+
+class IpSearch  {
+  
     private $prefStart=array(); 
     private $prefEnd=array(); 
     private $endArr=array(); 
     private $addrArr=array(); 
     private $fp;
+ 
+    static private   $instance= null;
 
-    function __construct($path) {
+    private    function __construct() { 
+        $this->loadDat(); 
+    }
+
+    private  function loadDat(){
+        $path='qqzeng-ip-3.0-ultimate.dat';
         $this->fp = fopen($path, 'rb');
         $fsize = filesize($path);
      
@@ -20,8 +33,8 @@ class IpSearch {
         for ($k = 0; $k < 256; $k++)
         {
             $i = $k * 8 + 4;           
-            $this->prefStart[$k] =unpack("V",$data,$i)[1];
-            $this->prefEnd[$k] =unpack("V",$data,$i+4)[1];               
+            $this->prefStart[$k]=unpack("V",$data,$i)[1];
+            $this->prefEnd[$k]=unpack("V",$data,$i+4)[1];               
         }
 
         $RecordSize =unpack("V",$data,0)[1];  
@@ -34,18 +47,35 @@ class IpSearch {
             $offset =  unpack("V", $data[4 + $p]. $data[5 + $p]. $data[6 + $p]  . "\x0")[1];
             $length =unpack("C", $data[7 + $p])[1];
           
-            fseek( $this->fp, $offset);
+            fseek($this->fp, $offset);
             $this->addrArr[$i] =fread( $this->fp,  $length);
         }
+
+
     }
 
-    function __destruct() {
+    function __destruct(){
         if ($this->fp !== NULL) {
             fclose($this->fp);
         }
     }
 
-    function get($ip) {
+    private  function __clone() {}
+    private  function __wakeup() {}
+        
+    public static function getInstance() {
+        if (self::$instance instanceof IpSearch ) {           
+            return self::$instance;
+        }
+        else{
+          
+            return self::$instance = new IpSearch();
+          
+        }
+       
+    }
+    
+    public function get($ip) {
         $val =sprintf("%u",ip2long($ip));
         $ip_arr = explode('.', $ip); 
         $pref = $ip_arr[0];
@@ -55,7 +85,7 @@ class IpSearch {
         return $this->addrArr[$cur];
     }
 
-    function BinarySearch($low, $high, $k) {
+    private function BinarySearch($low, $high, $k) {
         $M = 0;
         while ($low <= $high) {
             $mid = floor(($low + $high) / 2);
