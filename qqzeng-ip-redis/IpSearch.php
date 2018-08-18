447e58dce6b464053d1a6af6a332fbf7f0c0c6ef
qqzeng-ip <zengxiangzhan2010@163.com>
2018-08-19 01:29:23 +0800

    Redis 快速内存数据库 批量导入以及高性能查询
    Redis 快速内存数据库 批量导入以及高性能查询

---
+++ qqzeng-ip-redis/IpSearch.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-Type: text/json;charset=utf-8"');
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+
+//$t1 = microtime(true);
+
+
+$ip='78.138.53.9';
+$ipnum = ip2long($ip);
+
+$result=$redis->zRangeByScore('qqzengip', $ipnum, "inf", array( "limit" => array(0, 1) ));
+$k = array_keys($result);
+$k = $k[0];
+$area_info = $result[$k];
+
+
+list($start,$end,$startnum,$endnum,$continent,$country,$province, $city, $district,$isp,$areacode,$en,$cc,$lng,$lat) = explode("|", $area_info);
+
+$arr =array(
+			'code'=>0,
+				'data'=> array( 
+								'ip'=>$ip,
+								'continent'=>$continent,
+								'country'=>$country,
+								'province'=>$province,
+								'city'=>$city, 
+								'district'=>$district, 
+								'isp'=>$isp,
+								'areacode'=>$areacode,
+								'en'=>$en,
+								'cc'=>$cc,
+								'lng'=>$lng,
+								'lat'=>$lat
+								
+						)); 
+    echo   json_encode($arr); 
+    
+
+//$t2 = microtime(true);
+//echo '查询耗时'.($t2-$t1).'秒';
+    
+?>
