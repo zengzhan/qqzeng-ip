3606aed18a8a52f51ef70c4a35d53c71314cc556
qqzeng-ip <zengxiangzhan2010@163.com>
2018-12-16 21:16:11 +0800

    txt->mssql（导入和查询）IP数据库 行政区划数据库 号段数据库
    txt->mssql（导入和查询）IP数据库 行政区划数据库 号段数据库

--- mssql/area.sql
+++ mssql/area.sql
@@ -12,30 +12,30 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[areas](
-	[ID] [nvarchar](200) NULL,			--区划ID
+	[ID] [nvarchar](200) NULL,		--区划ID
 	[ParentId] [nvarchar](200) NULL,	--父ID
 	[LevelType] [nvarchar](200) NULL,	--级别
 	[Name] [nvarchar](200) NULL,		--全称
-	[ShortName] [nvarchar](200) NULL,	--简称
-	[Pinyin] [nvarchar](200) NULL,		--拼音
-	[Jianpin] [nvarchar](200) NULL,		--简拼
-	[FirstChar] [nvarchar](50) NULL,	--首拼
-	[CityCode] [nvarchar](200) NULL,	--区号
-	[ZipCode] [nvarchar](200) NULL,		--邮编
-	[Lng] [nvarchar](200) NULL,			--经度
-	[Lat] [nvarchar](200) NULL,			--纬度
-	[Remark1] [nvarchar](200) NULL,		--是否行政区
-	[Remark2] [nvarchar](200) NULL,		--类型（县级市|地级市|经济开发区|高新区|新区）
+	[ShortName] [nvarchar](200) NULL,	--简称	
 	[ParentPath] [nvarchar](100) NULL,	--ID路径（110000,110100,110105）
 	[Province] [nvarchar](50) NULL,		--省份全称
-	[City] [nvarchar](50) NULL,			--城市全称
+	[City] [nvarchar](50) NULL,		--城市全称
 	[District] [nvarchar](50) NULL,		--县区全称
 	[ProvinceShortName] [nvarchar](50) NULL,--省份简称
 	[CityShortName] [nvarchar](50) NULL,	--城市简称
 	[DistrictShortName] [nvarchar](50) NULL,--县区简称
 	[ProvincePinyin] [nvarchar](50) NULL,	--省份拼音
-	[CityPinyin] [nvarchar](50) NULL,		--城市拼音
-	[DistrictPinyin] [nvarchar](50) NULL	--县区拼音
+	[CityPinyin] [nvarchar](50) NULL,	--城市拼音
+	[DistrictPinyin] [nvarchar](50) NULL,	--县区拼音
+	[Pinyin] [nvarchar](200) NULL,		--拼音
+	[Jianpin] [nvarchar](200) NULL,		--简拼
+	[FirstChar] [nvarchar](50) NULL,	--首拼
+	[CityCode] [nvarchar](200) NULL,	--区号
+	[ZipCode] [nvarchar](200) NULL,		--邮编
+	[Lng] [nvarchar](200) NULL,		--经度
+	[Lat] [nvarchar](200) NULL,		--纬度
+	[Remark1] [nvarchar](200) NULL,		--是否行政区
+	[Remark2] [nvarchar](200) NULL,		--类型（县级市|地级市|经济开发区|高新区|新区）
 ) ON [PRIMARY]
 GO
 
