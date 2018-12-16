a5addd70d38bb669cca422f743e0fe2fbc1bfa0d
qqzeng-ip <zengxiangzhan2010@163.com>
2018-12-16 21:10:36 +0800

    txt->mssql（导入和查询）IP数据库 行政区划数据库 号段数据库
    txt->mssql（导入和查询）IP数据库 行政区划数据库 号段数据库

--- mssql/area.sql
+++ mssql/area.sql
@@ -2,29 +2,47 @@
 
 --创建 最新行政区划数据库  旗舰版
 
---字段 区划ID-父ID-全称-全称聚合-简称-简称聚合-级别-区号-邮编-拼音-简拼-首字母-经度-纬度-备注
+--字段 区划ID-父ID-级别-全称-简称-拼音-简拼-首拼-区号-邮编-经度-纬度-备注-类型-
+--    ID路径-省份全称-城市全称-县区全称-省份简称-城市简称-县区简称-省份拼音-城市拼音-县区拼音
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE TABLE [dbo].[areas](
-	[ID] [NVARCHAR](50) NULL,
-	[ParentId] [NVARCHAR](50) NULL,
-	[Name] [NVARCHAR](50) NULL,
-	[MergerName] [NVARCHAR](200) NULL,
-	[ShortName] [NVARCHAR](50) NULL,
-	[MergerShortName] [NVARCHAR](200) NULL,
-	[LevelType] [NVARCHAR](50) NULL,
-	[CityCode] [NVARCHAR](50) NULL,
-	[ZipCode] [NVARCHAR](50) NULL,
-	[Pinyin] [NVARCHAR](50) NULL,
-	[Jianpin] [NVARCHAR](50) NULL,
-	[FirstChar] [NVARCHAR](50) NULL,
-	[Lng] [NVARCHAR](50) NULL,
-	[Lat] [NVARCHAR](50) NULL,
-	[Remark] [NVARCHAR](50) NULL
-) 
+	[ID] [nvarchar](200) NULL,			--区划ID
+	[ParentId] [nvarchar](200) NULL,	--父ID
+	[LevelType] [nvarchar](200) NULL,	--级别
+	[Name] [nvarchar](200) NULL,		--全称
+	[ShortName] [nvarchar](200) NULL,	--简称
+	[Pinyin] [nvarchar](200) NULL,		--拼音
+	[Jianpin] [nvarchar](200) NULL,		--简拼
+	[FirstChar] [nvarchar](50) NULL,	--首拼
+	[CityCode] [nvarchar](200) NULL,	--区号
+	[ZipCode] [nvarchar](200) NULL,		--邮编
+	[Lng] [nvarchar](200) NULL,			--经度
+	[Lat] [nvarchar](200) NULL,			--纬度
+	[Remark1] [nvarchar](200) NULL,		--是否行政区
+	[Remark2] [nvarchar](200) NULL,		--类型（县级市|地级市|经济开发区|高新区|新区）
+	[ParentPath] [nvarchar](100) NULL,	--ID路径（110000,110100,110105）
+	[Province] [nvarchar](50) NULL,		--省份全称
+	[City] [nvarchar](50) NULL,			--城市全称
+	[District] [nvarchar](50) NULL,		--县区全称
+	[ProvinceShortName] [nvarchar](50) NULL,--省份简称
+	[CityShortName] [nvarchar](50) NULL,	--城市简称
+	[DistrictShortName] [nvarchar](50) NULL,--县区简称
+	[ProvincePinyin] [nvarchar](50) NULL,	--省份拼音
+	[CityPinyin] [nvarchar](50) NULL,		--城市拼音
+	[DistrictPinyin] [nvarchar](50) NULL	--县区拼音
+) ON [PRIMARY]
+GO
 
 
 
---导入数据库
+
+--导入数据库   txt 中文编码
 BULK INSERT dbo.[areas]
 FROM 'G:\IP数据库\areas.txt' 
 WITH (
