b601c585465c4bce7525f75653c8ec80c34a2c73
qqzeng-ip <zengxiangzhan2010@163.com>
2021-12-12 10:55:01 +0800

    手机号码归属地查询 php 解析 添加内存优化版
    手机号码归属地查询 php 解析 添加内存优化版

--- qqzeng-phone-php/PhoneSearch2.php
+++ qqzeng-phone-php/PhoneSearch2.php
@@ -1,119 +1,102 @@
 <?php
-namespace phone;
-// 2021-12 by  bao si  内存优化
-class PhoneSearch {
-    private $prefmap=[];
-    private $phoneArr=[];
-    private $addrArr=[];
-    private $ispArr=[]; 
+class PhoneSearch{
+// 2021-12 by  bao si  内存优化  查询标准版 支持  单个查询 和 批量查询
+    private $prefmap=[]; 
+    private $phonemap=[]; 
+    private $phoneArr=[]; 
+    private $addrArr=[]; 
+    private $ispArr=[];
     private $fp;
-    private $data = '';
-    private $prefSize = 0;
-    
-    public function __construct($filename = "phone.dat") {
-        $path = dirname(__FILE__).'/'.$filename;
-        $this->fp = null;
-        if (($this->fp = @fopen($path, 'rb')) !== false) {
-	        $fsize = filesize($path);
-	        $this->data = fread( $this->fp, $fsize); 
-	        $this->prefSize =$this->BytesToLong($this->data[0], $this->data[1], $this->data[2], $this->data[3]);
-	        $RecordSize =$this->BytesToLong($this->data[4], $this->data[5], $this->data[6], $this->data[7]);
-	        $descLength =$this->BytesToLong($this->data[8], $this->data[9], $this->data[10], $this->data[11]);
-	        $ispLength =$this->BytesToLong($this->data[12], $this->data[13], $this->data[14], $this->data[15]);
-	
-	        //内容数组
-	        $descOffset = 16 + $this->prefSize * 9 + $RecordSize * 7;
-	        fseek( $this->fp, $descOffset);
-	       
-	        $this->addrArr =explode('&',fread( $this->fp,  $descLength));
-	    
-	        //运营商数组
-	        $ispOffset = 16 + $this->prefSize * 9 + $RecordSize * 7 + $descLength;
-	        fseek( $this->fp, $ispOffset );
-	        $this->ispArr = explode('&',fread( $this->fp,  $ispLength));
-	
-	        //前缀区
-	        for ($k = 0; $k < $this->prefSize;$k++) {
-	            $i = $k * 9 + 16;
-	            $n = ord($this->data[$i]) & 0xFF;
-	            $this->prefmap[$n][0] = $this->BytesToLong($this->data[$i + 1], $this->data[$i + 2], $this->data[$i + 3], $this->data[$i + 4]);
-	            $this->prefmap[$n][1] =$this-> BytesToLong($this->data[$i + 5], $this->data[$i + 6], $this->data[$i + 7], $this->data[$i + 8]);
-	        }
-            register_shutdown_function([&$this, '__destruct']);
-        }
-    }
+    private $figureLen = 16;
 
-    private function getPhoneArr($low,$high){
-        for ($i = $low; $i < $high; $i++)
-        {
-            $p = 16 + $this->prefSize * 9 + ($i * 7);
-            $this->phoneArr[$i] = $this->BytesToLong($this->data[$p], $this->data[1 + $p], $this->data[2 + $p], $this->data[3 + $p]);
-        }
-    }
+    public function __construct($path) {
+        $this->fp = fopen($path, 'rb');
+        if(!$this->fp) die('Can\'t open qqzeng-phone.dat');
+        $figureData = fread($this->fp, $this->figureLen);
+        $figure_arr = str_split($figureData,4);
+        $prefSize =$this->bytes2Long($figure_arr[0][0], $figure_arr[0][1], $figure_arr[0][2], $figure_arr[0][3]);
+        $recordSize =$this->bytes2Long($figure_arr[1][0], $figure_arr[1][1], $figure_arr[1][2], $figure_arr[1][3]);
+        $addrLength =$this->bytes2Long($figure_arr[2][0], $figure_arr[2][1], $figure_arr[2][2], $figure_arr[2][3]);
+        $ispLength =$this->bytes2Long($figure_arr[3][0], $figure_arr[3][1], $figure_arr[3][2], $figure_arr[3][3]);
 
-    private function readInfo($mid){
-        $p = 16 + $this->prefSize * 9 + ($mid * 7);
-        $add_mid = $this->BytesToLong2($this->data[4 + $p],$this->data[5 + $p]);
-        $isp_mid = ord($this->data[6 + $p])<<0;
-        return compact('add_mid','isp_mid');
-    }
-
-    private function BinarySearch($low, $high, $k) {
-	
-	   if ($low > $high) {
-			return -1;
-		} else {
-			
-			$mid = ($low + $high)>>1;
-			$phoneNum = $this->phoneArr[$mid];
-			if ($phoneNum == $k) return (int)$mid;
-			else if ($phoneNum > $k) return $this->BinarySearch($low, $mid - 1, $k);
-			else return $this->BinarySearch($mid + 1, $high, $k);
-		}
-    }
-   
-    private function BytesToLong($a, $b, $c, $d) {
-        $iplong = (ord($a) << 0) | (ord($b) << 8) | (ord($c) << 16) | (ord($d) << 24);
-        if ($iplong < 0) {
-            $iplong+= 4294967296;//负数时
-        };
-        return $iplong;
-    }
-    
-    private function BytesToLong2($a, $b) {
-        $iplong = (ord($a) << 0) | (ord($b) << 8) ;
-        if ($iplong < 0) {
-            $iplong+= 4294967296;//负数时
-        };
-        return $iplong;
-    }
-
-    final public function get($phone) {
-        if(!$this->fp) return [];
-        $pref= (int)(substr($phone,0, 3));
-        $val = (int)(substr($phone,0,7));
-        $low = $this->prefmap[$pref][0]??0;
-        $high = $this->prefmap[$pref][1]??0;
-        $this->getPhoneArr($low,$high);
-		if ($high == 0 || !$this->phoneArr) {
-			return [];
-		}
-		$cur = $low == $high ? $low : $this->BinarySearch($low, $high, $val);
-		if ($cur != -1) {
-			$info = $this->readInfo($cur);
-			$results = $this->addrArr[$info['add_mid']].'|'.$this->ispArr[$info['isp_mid']];
-			list($province, $city,$areacode,$citycode, $postcode,$isp) = explode("|", $results);
-			return compact('province', 'city', 'isp', 'citycode' ,'postcode', 'areacode');
-		} else {
-			return [];
+        //前缀区
+        $prefOffset = ftell($this->fp);
+        fseek( $this->fp, $prefOffset);
+        $prefData = fread( $this->fp, $prefSize * 9);
+        $pref_arr = str_split($prefData, 9);
+        foreach($pref_arr as $v) {
+            $n = ord($v[0]);
+            $this->prefmap[$n][0] = $this->bytes2Long($v[1], $v[2], $v[3], $v[4]);
+            $this->prefmap[$n][1] =$this-> bytes2Long($v[5], $v[6], $v[7], $v[8]);
         }
         
-    }
+        //号码索引区
+        $recordOffset = ftell($this->fp);
+        fseek( $this->fp, $recordOffset);
+        $recordData = fread( $this->fp, $recordSize * 7);
+        $record_arr = str_split($recordData, 7);
+        $i = 0;
+        foreach($record_arr as $v) {
+            $roughly = $this->bytes2Long($v[0], $v[1], $v[2], $v[3]);
+            $this->phoneArr[$roughly] = $i;
+            $this->phoneMap[$i][0] =$this->bytes2Long($v[4],$v[5]);
+            $this->phoneMap[$i][1] = ord($v[6]);
+            $i++;
+        }
+
+        //地区数组
+        $addrOffset = ftell($this->fp);
+        fseek( $this->fp, $addrOffset);
+        $this->addrArr = explode('&',fread( $this->fp, $addrLength));
     
-    function __destruct() {
+        //运营商数组
+        $ispOffset = ftell($this->fp);
+        fseek( $this->fp, $ispOffset);
+        $this->ispArr = explode('&',fread( $this->fp, $ispLength));
+
+        register_shutdown_function([&$this, '__destruct']);
+    }
+
+    //单个查询
+    public function query(int|string $phone) {
+        $pref= (int)(substr($phone,0, 3));
+        $val = (int)(substr($phone,0,7));
+        
+        $low = $this->prefmap[$pref][0] ?? 0;
+        $high = $this->prefmap[$pref][1] ?? 0;
+        
+	   if ($high == 0) {
+            return '';
+	   }
+	   $cur = $this->phoneArr[$val] ?? '-1';
+	   if ($cur != -1) {
+            return $this->addrArr[$this->phoneMap[$cur][0]].'|'.$this->ispArr[$this->phoneMap[$cur][1]];
+	   } else {
+            return '';
+	   }
+    }
+
+    //批量查询
+    public function batchQuery(array $phones = []) {
+        if(is_array($phones) && count($phones)){
+            return array_map('self::query',$phones);
+        }else{
+            return [];
+        }
+    }
+
+    private function bytes2Long(...$args) {
+        $long = $i = 0;
+        foreach($args as $v){
+            $long += (ord($v)) << ($i++*8);
+        }
+        return $long;
+    }
+
+    private function __destruct() {
         if ($this->fp) {
             fclose($this->fp);
         }
-        $this->fp = NULL;
+        $this->fp = null;
     }
 }
