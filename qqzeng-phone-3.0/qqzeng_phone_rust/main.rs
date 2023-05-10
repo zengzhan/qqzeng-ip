fc4d91d28139f1184675ae07b68da386bf4b898c
qqzeng-ip <zengxiangzhan2010@163.com>
2023-05-10 15:01:15 +0800

    手机号码归属地查询 rust 解析 3.0 内存优化版
    手机号码归属地查询 rust 解析 3.0 内存优化版

---
+++ qqzeng-phone-3.0/qqzeng_phone_rust/main.rs
@@ -0,0 +1,16 @@
+
+mod qqzeng_phone_search;
+fn main() {
+
+  let phone_search = qqzeng_phone_search::PHONE_SEARCH.lock().unwrap();
+  // 查询号段归属地
+  let phone = "13382401245";
+  let location = phone_search.query(phone);
+
+  // 输出结果
+
+  println!("The location of {} is {}", phone, location);
+  //江苏|南京|210000|025|320100|电信
+  
+
+}
