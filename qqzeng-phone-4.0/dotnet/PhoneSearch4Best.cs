5350d6805aecd18bb5dddb4f5671c4bb1fa29fdf
qqzeng-ip <zengxiangzhan2010@163.com>
2024-03-09 17:51:15 +0800

    最新4.0版，号段归属地DAT结构，文件更小，每秒解析1700万
    最新4.0版，号段归属地DAT结构，文件更小，每秒解析1700万

--- qqzeng-phone-4.0/dotnet/PhoneSearch4Best.cs
+++ qqzeng-phone-4.0/dotnet/PhoneSearch4Best.cs
@@ -8,66 +8,73 @@
 
      }
 
-     private int[] prefArr = new int[200];
-     private byte[] data;
-     private string[] addrArr;
-     private string[] ispArr;
+       private byte[] data;
+       private int[] prefArr;
+       private string[] addrArr;
+       private string[] ispArr;
 
-     /// <summary>
-     /// 初始化二进制dat数据
-     /// </summary>
-     /// <param name="dataPath"></param>
-     /// 
-
- public void LoadDat()
- {
-
-     var datPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"qqzeng-phone-4.0.dat");
-
-     data = File.ReadAllBytes(datPath);
-
-     uint[] a = new uint[5];
-     for (int i = 0; i < 5; i++)
-     {
-         a[i] = BitConverter.ToUInt32(data, i * 4);
-     }
-
-     var h = 20;
-     int startIndex = (int)(h + a[2] + a[3]);
-   
-     addrArr = Encoding.UTF8.GetString(data, h, (int)a[2]).Split('&'); 
-     ispArr = Encoding.UTF8.GetString(data, h + (int)a[2], (int)a[3]).Split('&');
-
-     for (var m = 0; m < a[0]; m++)
-     {
-         int i = m * 5 + startIndex;              
-         prefArr[data[i]] = (int)BitConverter.ToUInt32(data, i + 1); 
-     }
+       /// <summary>
+       /// 初始化二进制dat数据
+       /// </summary>
+       /// <param name="dataPath"></param>
+       /// 
 
 
- }
+       public void LoadDat()
+       {
+
+           var datPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"qqzeng-phone-4.0.dat");
+
+           data = File.ReadAllBytes(datPath);
+
+           uint[] a = new uint[5];
+           for (int i = 0; i < 5; i++)
+           {
+               a[i] = BitConverter.ToUInt32(data, i * 4);
+           }
+
+           var h = 20;
+           int startIndex = (int)(h + a[2] + a[3]);
+         
+           addrArr = Encoding.UTF8.GetString(data, h, (int)a[2]).Split('&'); 
+           ispArr = Encoding.UTF8.GetString(data, h + (int)a[2], (int)a[3]).Split('&');
+
+           prefArr = new int[200];
+           for (var m = 0; m < a[0]; m++)
+           {
+               int i = m * 5 + startIndex;              
+               prefArr[data[i]] = (int)BitConverter.ToUInt32(data, i + 1); 
+           }
+
+
+       }
 
 
 
 
 
- public string Query(string phone)
- {
-     var prefix = Convert.ToInt32(phone.Substring(0, 3));
-     var suffix = Convert.ToInt32(phone.Substring(3, 4));
-     var offset = prefix < 0xC8 ? prefArr[prefix] : 0;
-     if (offset > 0)
-     {
-         var n = BitConverter.ToUInt16(data, offset + (suffix << 1));
-         return addrArr[n >> 5] + "|" + ispArr[n & 0x001F];
-     }
-     else
-     {
-         return "不存在";
-     }
+       public string Query(string phone)
+       {
+           var prefix = Convert.ToInt32(phone.Substring(0, 3));
+           var suffix = Convert.ToInt32(phone.Substring(3, 4));
+           var offset = prefix < 0xC8 ? prefArr[prefix] : 0;
+           if (offset > 0)
+           {
+               var n = BitConverter.ToUInt16(data, offset + (suffix << 1));
+               return addrArr[n >> 5] + "|" + ispArr[n & 0x001F];
+           }
+           else
+           {
+               return "不存在";
+           }
 
 
- }
+       }
+
+
+
+
+   }
 
 
 
